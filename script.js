36245992083042aaf2ffe3a3931b7b59fde31873
maniARJ1 <arf22297@spengergasse.at>
2023-09-12 10:33:58 +0200

    bla bla

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function addElement() {
     li.innerHTML = "Neues Element";
     ul.appendChild(li);
 }
+
+
+function ausgabe() {
+        
+    var eingabeWert = document.getElementById("textfeld").value;
+    var liste = document.getElementById("meineUL");
+            var listItem = document.createElement("li");
+            listItem.textContent = eingabeWert;
+            liste.appendChild(listItem);
+
+        textfeld.value = "";
+    
+    }
