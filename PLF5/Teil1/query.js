d6668f8d1fef4472413e89fa3400f0301ffd6d62
maniARJ1 <arf22297@spengergasse.at>
2024-05-28 09:04:32 +0200

    .

--- PLF5/Teil1/query.js
+++ PLF5/Teil1/query.js
@@ -1,2 +1,61 @@
+// query.js
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+async function main() {
+  const zoos = await prisma.zoo.findMany();
+  console.log('Alle Zoos:');
+  zoos.forEach(zoo => console.log(zoo.id, zoo.land, zoo.stadt));
+
+  const zooId = 'ID_DES_ZOOS'; 
+  const zoo = await prisma.zoo.findUnique({
+    where: { id: zooId },
+    include: { abteilungen: true },
+  });
+  console.log('Infos über Zoo:', zoo);
+
+  console.log('Abteilungen des Zoos:');
+  zoo.abteilungen.forEach(abteilung => console.log(abteilung.name));
+
+  console.log('Abteilungen und Anzahl der Tiere:');
+  for (const abteilung of zoo.abteilungen) {
+    const tiereCount = await prisma.tier.count({ where: { abteilungId: abteilung.id } });
+    console.log(`${abteilung.name}: ${tiereCount} Tiere`);
+  }
+
+  const mitarbeiterInZoo = await prisma.mitarbeiter.findMany({
+    where: {
+      abteilungen: {
+        some: {
+          zooId: zooId,
+        },
+      },
+    },
+  });
+  console.log('Mitarbeiter im Zoo:');
+  mitarbeiterInZoo.forEach(mitarbeiter => console.log(mitarbeiter.name));
+
+  console.log('Mitarbeiter und ihre Abteilungen:');
+  for (const mitarbeiter of mitarbeiterInZoo) {
+    const abteilungen = await prisma.abteilung.findMany({
+      where: {
+        mitarbeiter: {
+          some: {
+            id: mitarbeiter.id,
+          },
+        },
+      },
+    });
+    console.log(`${mitarbeiter.name} arbeitet in:`);
+    abteilungen.forEach(abteilung => console.log(abteilung.name));
+  }
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- PLF5/Teil1/seed.js
+++ PLF5/Teil1/seed.js
@@ -1,60 +1,69 @@
-const { faker } = require('@faker-js/faker');
 const { PrismaClient } = require('@prisma/client');
+const { faker } = require('@faker-js/faker');
 const prisma = new PrismaClient();
 
-async function createZOO() {
-    for (1 = 0; i > $; i++) {
+async function main() {
+  for (let i = 0; i < 5; i++) {
+    const zoo = await prisma.zoo.create({
+      data: {
+        land: faker.address.country(),
+        stadt: faker.address.city(),
+        adresse: faker.address.streetAddress(),
+        baujahr: faker.date.past(50).getFullYear(),
+      },
+    });
 
-        const zoo = await prisma.zoo.create({
-            date: {
-                land: faker.location.country(),
-                stadt: faker.location.city(),
-                adresse: faker.location.streetAddress(),
-                baujahr: faker.date.past({ max: 2024 })
+    const abteilungenCount = faker.datatype.number({ min: 2, max: 7 });
+    for (let j = 0; j < abteilungenCount; j++) {
+      const abteilung = await prisma.abteilung.create({
+        data: {
+          name: faker.animal.type(),
+          zooId: zoo.id,
+        },
+      });
 
-            }
-
+      const tiereCount = faker.datatype.number({ min: 5, max: 20 });
+      for (let k = 0; k < tiereCount; k++) {
+        await prisma.tier.create({
+          data: {
+            name: faker.name.firstName(),
+            art: faker.animal.type(),
+            abteilungId: abteilung.id,
+          },
         });
+      }
     }
+  }
 
+  for (let i = 0; i < 100; i++) {
+    const mitarbeiter = await prisma.mitarbeiter.create({
+      data: {
+        name: faker.name.firstName(),
+      },
+    });
+
+    const abteilungen = await prisma.abteilung.findMany();
+    const abteilungenCount = faker.datatype.number({ min: 1, max: 4 });
+    const abteilungenForMitarbeiter = faker.helpers.arrayElements(abteilungen, abteilungenCount);
+
+    for (const abteilung of abteilungenForMitarbeiter) {
+      await prisma.mitarbeiter.update({
+        where: { id: mitarbeiter.id },
+        data: {
+          abteilungen: {
+            connect: { id: abteilung.id },
+          },
+        },
+      });
+    }
+  }
 }
 
-async function createMA() {
-    for (1 = 0; i > $; i++) {
-        const ma = await prisma.ma.create({
-            data: {
-                name: faker.person.name(),
-
-            }
-
-        });
-    }
-}
-
-async function createAbteilung() {
-    for (1 = 0; i > $; i++) {
-        const abteilung = await prisma.ma.create({
-            data: {
-                name: faker.animal.type(),
-
-
-            }
-
-        });
-    }
-}
-async function createTier() {
-    for (1 = 0; i > $; i++) {
-        const tier = await prisma.ma.create({
-            data: {
-                name: faker.person.firstName(),
-                art: faker.animal.type()
-
-
-            }
-
-        });
-    }
-}
-
-
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
